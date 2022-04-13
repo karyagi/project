3012188f179f40e3dfd0b42b6bea45f9117e1d52
Abueyub <abueyub1@gmail.com>
2022-04-13 20:43:20 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+"use strict";
+
+
+
+//________________________________
+// console.log('arr' + ' - object');
+// console.log(4 + +'5');
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++; // постфиксная
+// decr--;
+// console.log(incr);
+// console.log(decr);
+
+// ++incr;// префиксная
+// --decr;
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+// console.log(2*4 === '8');
+// console.log(2 + 2 * 2 === 8);
+// console.log(2 + 2 * 2 != 8);
+// console.log(2 + 2 * 2 !== '6');
+
+// const isChecked = false,
+//       isClose = false;
+
+// console.log(isChecked && isClose);
+// console.log(isChecked || isClose);
+// console.log(isChecked || !isClose);
+
+//_____________________________
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Hello, ${ user}`);
+
+//_____________________________
+// alert('Hello');
+
+//  const result = confirm("Are you here?");
+//  console.log(result);
+
+// const answer = +prompt('is your 18 age?', '18');
+// console.log(typeof(answer));//вся информация от пользователя будет ввиде строк
+
+// const answers = [];
+
+// answers[0] = prompt('What is your first name?', '');
+// answers[1] = prompt('What is your last name?', '');
+// answers[2] = prompt('How are you old?', '');
+
+// document.write(answers);
+// console.log(typeof(answers));
+// console.log(typeof(null));
